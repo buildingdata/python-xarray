0b4b5928f16ee881fac7d06a1d79419d01c5caa2
Haoning Wu <whn@nousbuild.com>
2022-08-18 10:18:59 +0800

    🌍 优化逻辑支持全球经纬度

--- nc-逐时.py
+++ nc-逐时.py
@@ -13,16 +13,11 @@ import os
 path = "2018//"
 files = os.listdir(path)
 
-# 设定一个经纬度
-loc_lat = -30
-loc_lon = 110
-
 # 保证只写入一次标头
 header_added = False
 
-# 提取 nc 文件名称中的日期信息
 for file in files:
-    # 这里要根据文件名修改
+    # 提取 nc 文件名称中的日期信息（这里要根据文件名修改）
     file_year = file[27:31]
     file_month = file[32:34]
     file_day = file[35:37]
@@ -31,32 +26,44 @@ for file in files:
 
     # 依次打开所有的 nc 文件
     ds = xr.open_dataset(path + file)
-    this_ds = ds.sel(lat=loc_lat, lon=loc_lon)
 
-    # 依次导出到 csv
-    with open("out-逐时.csv", "a", newline="") as csvfile:
+    # 历遍所有经纬度（步长 1°）
+    for i in range(-90, 90, 1):
+        loc_lat = i
+        for j in range(-180, 180, 1):
+            loc_lon = j
 
-        # 标头要根据 nc 文件进行适当修改
-        fieldnames = ["Year", "Month", "Day", "Hour", "Rs"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            this_ds = ds.sel(lat=loc_lat, lon=loc_lon)
 
-        # 保证只写入一次标头
-        if not header_added:
-            writer.writeheader()
-            header_added = True
+            # 依次导出到 csv
+            with open("out-逐时.csv", "a", newline="") as csvfile:
 
-        # 写入每个 nc 文件的值
-        writer.writerow(
-            {
-                "Year": file_year,
-                "Month": file_month,
-                "Day": file_day,
-                "Hour": file_hour,
-                "Rs": this_ds.global_radiation.data,
-                # "temp": this_ds.temp.data,
-            }
-        )
+                # 标头要根据 nc 文件进行适当修改
+                fieldnames = ["Year", "Month", "Day", "Hour", "Rs", "Lat", "Lon"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+                # 保证只写入一次标头
+                if not header_added:
+                    writer.writeheader()
+                    header_added = True
+                    print("正在写入请稍后，等待完成通知...")
+
+                # 写入每个 nc 文件的值
+                writer.writerow(
+                    {
+                        "Year": file_year,
+                        "Month": file_month,
+                        "Day": file_day,
+                        "Hour": file_hour,
+                        "Rs": this_ds.global_radiation.data,
+                        "Lat": loc_lat,
+                        "Lon": loc_lon,
+                        # "temp": this_ds.temp.data,
+                    }
+                )
+                
     # 关闭 nc 文件
     print(file + "已经写入完成！")
     xr.Dataset.close(ds)
+
+print("所有文件已完成写入！")
